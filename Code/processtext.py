1adc2d32da4b0330c2453f3a9ee105aacb1ce5ca
AM Steen <mubbymom@centralaveconsult.com>
2020-07-14 21:03:51 -0500

    added MakeCSV() and added comment to MakeSection()

--- Code/processtext.py
+++ Code/processtext.py
@@ -13,7 +13,7 @@ def ProcessText(FilePath):
     pattern = re.compile(r"[^A-Z\u00C0-\u00DF]")
     return(re.sub(pattern, "", string))
 
-def MakeSection(ProcessedText, MinLength = 20, MaxLength = 50):
+def MakeSection(ProcessedText, MinLength = 20, MaxLength = 21):
     """
     input:
         1) string of English, German, or Spanish text (all caps, no punctuation)
@@ -21,6 +21,9 @@ def MakeSection(ProcessedText, MinLength = 20, MaxLength = 50):
         2) MinLength = minimum length of returned section
         3) MaxLength = maximum length of returned section
     output: a section of that file (string) with a random length
+
+    MaxLength must be > MinLength. If you want all same-sized sections, set
+        MaxLength = MinLength + 1
     """
     MaxPosition = len(ProcessedText)
     StringLength = np.random.randint(MinLength, MaxLength)
@@ -61,6 +64,20 @@ def TxtToTupleList(FilePath, Language):
             f"Allowed languages are: {', '.join(LangDict.keys())}")
     ProcessedText = ProcessText(FilePath)
     return(MakeTupleList(ProcessedText, LangDict[Language]))
+
+def MakeCSV(TupleList):
+    """
+    input:
+    output:
+    """
+    BaseFilePath = Path("/Users/Aidia/Documents/SummerResearch2020/LanguageNeuralNet/Data/")
+    langs = ["English", "German", "Spanish"]
+    MasterList = []
+    for lang in langs:
+            FileName = f"{lang}Text.txt"
+            FilePath = BaseFilePath / FileName
+            MasterList.extend(TxtToTupleList(FilePath, lang))
+    return(MasterList)
 
 if __name__ == "__main__":
 
