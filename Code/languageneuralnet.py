a34337978b1c70c3676cd5e8a8d49eb6348488f2
AM Steen <mubbymom@centralaveconsult.com>
2020-07-19 17:59:31 -0500

    Added splitting input into random train and test sets

--- Code/languageneuralnet.py
+++ Code/languageneuralnet.py
@@ -2,6 +2,7 @@ from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 import pandas as pd
+import numpy as np
 import splitstring
 from sklearn.preprocessing import OneHotEncoder
 
@@ -9,7 +10,16 @@ FileName = "/Users/Aidia/Documents/SummerResearch2020/LanguageNeuralNet/Data/Mas
 X = splitstring.LetArrayToNumArray(splitstring.CSVToNumpy(FileName))
 Y = splitstring.GetLanguageIndex(FileName)
 
+TrainPercent = 0.9
+np.random.seed(100)
+ShuffleIndex = np.random.permutation(Y.shape[0])
+XShuffled, YShuffled = X[ShuffleIndex], Y[ShuffleIndex]
+SplitBorder = int(np.round(TrainPercent * X.shape[0]))
 
+XTrain = XShuffled[:SplitBorder]
+YTrain = YShuffled[:SplitBorder]
+XTest = XShuffled[SplitBorder:]
+YTest = YShuffled[SplitBorder:]
 
 # encoder = OneHotEncoder(categories='auto')
 # Y_OneHot = encoder.fit_transform(Y.reshape(-1, 1))
@@ -22,7 +32,7 @@ model.add(Dense(1, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X, Y, epochs = 150, batch_size = 10)
+model.fit(XTrain, YTrain, epochs = 150, batch_size = 10)
 
 # _, accuracy = model.evaluate(X, Y)
 # print('Accuracy: %.2f' % (accuracy*100))
